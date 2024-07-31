7c98d6c1623b138de7ce4946283597042726b1ae
Carlos Encine <carlos.encine@outlook.com>
2024-07-31 20:12:15 -0300

    feat: Add mongodb support

---
+++ src/Encine.IoTMonitor.Api/Extensions/DatabaseExtension.cs
@@ -0,0 +1,19 @@
+﻿using Encine.IoTMonitor.Infrastructure.Adapters.Data;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
+
+namespace Encine.IoTMonitor.Api.Extensions
+{
+    public static class DatabaseExtension
+    {
+        public static void AddDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetConnectionString("MongoDb"));
+
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseMongoDB(client, configuration.GetSection("MongoDb:Database").Value);
+            });
+        }
+    }
+}
---
+++ src/Encine.IoTMonitor.Infrastructure/Adapters/Data/DataContext.cs
@@ -0,0 +1,35 @@
+﻿using Encine.IoTMonitor.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
+using MongoDB.EntityFrameworkCore.Extensions;
+
+namespace Encine.IoTMonitor.Infrastructure.Adapters.Data
+{
+    public class DataContext : DbContext
+    {
+        //public static DataContext Create(IMongoDatabase database)
+        //{
+        //    return new(new DbContextOptionsBuilder<DataContext>()
+        //        .UseMongoDB(database.Client, database.DatabaseNamespace.DatabaseName)
+        //        .Options);
+        //}
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Sensor>().ToCollection("sensors");
+            modelBuilder.Entity<SensorData>().ToCollection("sensorData");
+            modelBuilder.Entity<DeviceConfiguration>().ToCollection("deviceConfigurations");
+        }
+
+        public DbSet<Sensor> Sensors { get; set; }
+        public DbSet<SensorData> SensorData { get; set; }
+        public DbSet<DeviceConfiguration> DeviceConfigurations { get; set; }
+    }
+    
+}
