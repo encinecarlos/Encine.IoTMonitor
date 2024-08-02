16bab7edb5e0bd970aaa24a3b192ad02005cbdcf
Carlos Encine <carlos.encine@outlook.com>
2024-08-02 08:58:10 -0300

    feat: Add controller for device entity

---
+++ src/Encine.IoTMonitor.Api/Controllers/SensorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Encine.IoTMonitor.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SensorController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllSensors()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSensor()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSensorById(Guid id)
+        {
+            return Ok();
+        }
+    }
+}
